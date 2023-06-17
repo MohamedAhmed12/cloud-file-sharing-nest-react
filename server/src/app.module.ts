2a129cb52a16264abc25c866ecd01793d4c5ce50
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-06-17 14:39:17 +0300

    feat(graphql): config graphql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,14 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
 
 const ormConfig = require('../ormconfig.json');
 
 @Module({
-  imports: [UserModule,
-
-    TypeOrmModule.forRoot(ormConfig[0])
-],
+  imports: [
+    UserModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      context: ({ req, connection }) => {
+        if (connection) {
+          console.log('>>>>>>>>>>>', connection, '>>>>>>>>>>');
+          return { headers: connection.context };
+        }
+        return { headers: req.headers };
+      },
+      debug: true,
+      installSubscriptionHandlers: true,
+    }),
+    TypeOrmModule.forRoot(ormConfig[0]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
