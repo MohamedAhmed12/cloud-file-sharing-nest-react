ebd4cb5514be2e8d45b054a86364893d0d1148af
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-06-20 07:01:44 +0300

    reformating

--- server/src/user/user.entity.ts
+++ server/src/user/user.entity.ts
@@ -3,49 +3,49 @@ import { hash } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { config } from 'src/shared/config';
 import {
-  BaseEntity,
-  BeforeInsert,
-  Column,
-  CreateDateColumn,
-  Entity,
-  PrimaryGeneratedColumn,
+    BaseEntity,
+    BeforeInsert,
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
 } from 'typeorm';
 
 @Entity('users')
 @ObjectType()
 export class User extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  @Field(() => ID)
-  id: string;
+    @PrimaryGeneratedColumn()
+    @Field(() => ID)
+    id: string;
 
-  @Column('text')
-  @Field()
-  name: string;
+    @Column('text')
+    @Field()
+    name: string;
 
-  @Column('text', { unique: true })
-  @Field()
-  email: string;
+    @Column('text', { unique: true })
+    @Field()
+    email: string;
 
-  @Column('text')
-  password: string;
+    @Column('text')
+    password: string;
 
-  @CreateDateColumn()
-  @Field()
-  created_at: Date;
+    @CreateDateColumn()
+    @Field()
+    created_at: Date;
 
-  @CreateDateColumn()
-  @Field()
-  updated_at: Date;
+    @CreateDateColumn()
+    @Field()
+    updated_at: Date;
 
-  private get AuthToken(): string | undefined {
-    const { id, name } = this;
-    return sign({ id, name }, config.JWT_TOKEN, {
-      expiresIn: config.JWT_TOKEN_EXPIRATION,
-    });
-  }
+    get auth_token(): string | undefined {
+        const { id, name } = this;
+        return sign({ id, name }, config.JWT_TOKEN, {
+            expiresIn: config.JWT_TOKEN_EXPIRATION,
+        });
+    }
 
-  @BeforeInsert()
-  async hashPass() {
-    this.password = await hash(this.password, 12);
-  }
+    @BeforeInsert()
+    async hashPass() {
+        this.password = await hash(this.password, 12);
+    }
 }
